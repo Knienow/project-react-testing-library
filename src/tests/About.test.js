33c32f83d0e4a573662c0937704e9fbaaae79ddb
Katia Nienow <nienowkatia@gmail.com>
2023-03-28 17:29:12 -0300

    requisito 2

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -1,1 +1,29 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+// import userEvent from '@testing-library/user-event';
+import renderWithRouter from '../renderWithRouter';
+import About from '../pages/About';
+
+describe('Verifica se a página About contém', () => {
+  it('informações sobre a Pokédex', () => {
+    renderWithRouter(<About />);
+    const paragraphs = screen.getAllByText(/Pokémon/i);
+    expect(paragraphs).toHaveLength(2);
+  });
+  it('um heading h2 com o texto "About Pokédex"', () => {
+    renderWithRouter(<About />);
+
+    const aboutTitle = screen.getByRole(
+      'heading',
+      { name: 'About Pokédex' },
+    );
+    expect(aboutTitle).toBeInTheDocument();
+  });
+
+  it('a imagem de uma Pokédex correspondente a https://cdn2.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png', () => {
+    renderWithRouter(<About />);
+    const image = screen.getByAltText('Pokédex');
+    expect(image).toBeInTheDocument();
+    expect(image.src).toBe('https://cdn2.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png');
+  });
+});
